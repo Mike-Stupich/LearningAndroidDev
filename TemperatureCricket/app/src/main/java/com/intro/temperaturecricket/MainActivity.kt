edcc5b8599851af30e33ca3d3df81ea21941529c
Mike Stupich <mike.stupich@gmail.com>
2019-06-14 22:47:24 -0400

    Completed cricket temperature challenge

--- TemperatureCricket/app/src/main/java/com/intro/temperaturecricket/MainActivity.kt
+++ TemperatureCricket/app/src/main/java/com/intro/temperaturecricket/MainActivity.kt
@@ -2,11 +2,26 @@ package com.intro.temperaturecricket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvTempRes.visibility = (View.GONE)
+
+        btnSubmitTemp.setOnClickListener{
+            val numChirps = etNumChirps.text.toString().toFloat()
+            tvTempRes.text = String.format(getString(R.string.degree_result), calcTemp(numChirps))
+            tvTempRes.visibility = (View.VISIBLE)
+        }
     }
+
+    private fun calcTemp(numChirps: Float):Float {
+        return (numChirps / 3 + 4)
+    }
+
 }
