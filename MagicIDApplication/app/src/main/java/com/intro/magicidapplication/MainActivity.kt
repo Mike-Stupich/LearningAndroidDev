f229636ecfb6e25af4bbc9a7f50f226078e4e109
Mike Stupich <mike.stupich@gmail.com>
2019-06-14 23:25:30 -0400

    Error handling in ID app

--- MagicIDApplication/app/src/main/java/com/intro/magicidapplication/MainActivity.kt
+++ MagicIDApplication/app/src/main/java/com/intro/magicidapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.intro.magicidapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,16 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         btnIdSubmit.setOnClickListener {
             val idNum = etID.text.toString().trim()
-            val dob = idNum.substring(0, 6)
-            val sex = idNum[6].toString().toInt()
-            val sGender = if (sex < 5) "Female" else "Male"
-            val nationality = idNum[10].toString().toInt()
-            val sNationality = if (nationality == 0) "SA Citizen" else "Permanent Resident"
-            tvResults.text = String.format("%s\n%s\n%s",
-                String.format(getString(R.string.dob), dob),
-                String.format(getString(R.string.gender), sGender),
-                String.format(getString(R.string.nationality), sNationality))
-            tvResults.visibility = View.VISIBLE
+            if (idNum.length < 10) {
+                Toast.makeText(this@MainActivity, "Please enter a valid 13 digit ID", Toast.LENGTH_SHORT).show()
+            } else {
+                val dob = idNum.substring(0, 6)
+                val sex = idNum[6].toString().toInt()
+                val sGender = if (sex < 5) "Female" else "Male"
+                val nationality = idNum[10].toString().toInt()
+                val sNationality = if (nationality == 0) "SA Citizen" else "Permanent Resident"
+                tvResults.text = String.format("%s\n%s\n%s",
+                    String.format(getString(R.string.dob), dob),
+                    String.format(getString(R.string.gender), sGender),
+                    String.format(getString(R.string.nationality), sNationality))
+                tvResults.visibility = View.VISIBLE
+            }
+
         }
 
     }
